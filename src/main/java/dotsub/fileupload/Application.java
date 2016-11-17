1c789202c980936301aead569a16cfe627dd076f
Craig Doremus <cdoremus@gmail.com>
2016-11-17 09:21:29 -0500

    Added configs for storageService and storageProperties

--- src/main/java/dotsub/fileupload/Application.java
+++ src/main/java/dotsub/fileupload/Application.java
@@ -1,5 +1,6 @@
 package dotsub.fileupload;
 
+import dotsub.fileupload.service.FileSystemStorageService;
 import dotsub.fileupload.service.StorageProperties;
 import dotsub.fileupload.service.StorageService;
 
@@ -8,9 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
+//@Import(HibernateDataConfig.class)
 public class Application {
 
     public static void main(String[] args) {
@@ -25,6 +28,17 @@ public class Application {
             storageService.init();
 		};
 	}
+
+	@Bean
+	StorageService storageService() {
+		return new FileSystemStorageService(storageProperties());
+	}
+	
+	@Bean
+	StorageProperties storageProperties() {
+		return new StorageProperties();
+	}
+	
 
 }
 
