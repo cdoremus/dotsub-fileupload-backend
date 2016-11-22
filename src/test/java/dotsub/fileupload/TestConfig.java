5b438bef20186ef39cfbf34f5f8406c36cc77c37
Craig Doremus <cdoremus@gmail.com>
2016-11-21 19:15:13 -0500

    Removed unused imports

--- src/test/java/dotsub/fileupload/TestConfig.java
+++ src/test/java/dotsub/fileupload/TestConfig.java
@@ -3,6 +3,7 @@ package dotsub.fileupload;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 
 import dotsub.fileupload.service.StorageProperties;
 
