41373d6e371712adda24f38c2d981a6ca3cf42bc
Craig Doremus <cdoremus@gmail.com>
2016-11-21 20:49:28 -0500

    Removed references to deleted files

--- src/main/java/dotsub/fileupload/Application.java
+++ src/main/java/dotsub/fileupload/Application.java
@@ -1,30 +1,15 @@
 package dotsub.fileupload;
 
-import dotsub.fileupload.service.StorageProperties;
-import dotsub.fileupload.service.StorageService;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-
-	@Bean
-	CommandLineRunner init(StorageService fileUploadService) {
-		return (args) -> {
-            fileUploadService.deleteAll();
-            fileUploadService.init();
-		};
-	}
 
 }
 
