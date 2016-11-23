a09c2ce08c85faaded9c20d2fd1512efefd4d288
Craig Doremus <cdoremus@gmail.com>
2016-11-23 16:42:32 -0500

    Added @Transactional annotation to findBy*() methods.

--- src/main/java/dotsub/fileupload/repository/FileUploadMetatdataRepository.java
+++ src/main/java/dotsub/fileupload/repository/FileUploadMetatdataRepository.java
@@ -1,12 +1,16 @@
 package dotsub.fileupload.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface FileUploadMetatdataRepository extends JpaRepository<FileUploadMetadata, Long> {
 
+	@Transactional(readOnly = true)
 	FileUploadMetadata findByTitle(String title);
 
+	@Transactional(readOnly = true)
 	FileUploadMetadata findByFilename(String fileName);
 }
